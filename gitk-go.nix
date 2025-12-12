4c13d6d763993ee25f71e0ab5071c0d20b3ee344
Thiago Kenji Okada <thiagokokada@gmail.com>
2025-12-12 22:45:59 +0000

    gitk-go.nix: make binaries produced via it run in non-NixOS systems

--- gitk-go.nix
+++ gitk-go.nix
@@ -37,30 +37,53 @@ buildGoModule {
 
   env.GOEXPERIMENT = "greenteagc";
 
-  postFixup = (
-    lib.concatStringsSep " " [
-      "wrapProgram $out/bin/gitk-go"
-      (lib.optionalString stdenv.isLinux "--set LD_LIBRARY_PATH ${
-        lib.makeLibraryPath [
-          # XXX: not sure if all those libs are necessary
-          xorg.libX11
-          xorg.libXext
-          xorg.libXft
-          xorg.libXrender
-          xorg.libXfixes
-          xorg.libXcursor
-          xorg.libXinerama
-          xorg.libXrandr
-          fontconfig
-          freetype
-          libpng
-          libjpeg
-          zlib
-        ]
-      }")
-      (lib.optionalString withGitCli "--prefix PATH : ${lib.getExe git}")
-    ]
-  );
+  postFixup =
+    let
+      linuxLibs = lib.makeLibraryPath [
+        xorg.libX11
+        xorg.libXext
+        xorg.libXrender
+        xorg.libXft
+        xorg.libXfixes
+        xorg.libXcursor
+        xorg.libXinerama
+        xorg.libXrandr
+        xorg.libXScrnSaver
+        fontconfig
+        freetype
+        libpng
+        libjpeg
+        zlib
+      ];
+
+      gitPath = lib.optionalString withGitCli "--prefix PATH : ${lib.makeBinPath [ git ]}";
+    in
+    ''
+      mv $out/bin/gitk-go $out/bin/.gitk-go-unwrapped
+    ''
+    +
+      # XXX: purego (used by modernc.org/tk9.0) is kinda cursed, it makes the Go
+      # binaries to be linked to the dynamic linker even when CGO is disabled.
+      # The issue is that the libraries that are shipped with tk9.0 also get
+      # linked with the dynamic linker used by Go, but in non-NixOS systems
+      # Go will get confused trying to load the libs from Nix with the system
+      # dynamic loader (and this doesn't work).
+      # Ideally we would patch purego/tk9.0 to allow overriding this, but for
+      # now forcing the binary to load with Nix's dynnamic linker, even if this
+      # is an unholy hack.
+      lib.optionalString stdenv.isLinux ''
+        makeWrapper ${stdenv.cc.bintools.dynamicLinker} \
+          $out/bin/gitk-go \
+          --add-flags $out/bin/.gitk-go-unwrapped \
+          --argv0 gitk-go \
+          --set LD_LIBRARY_PATH ${linuxLibs} \
+          ${gitPath}
+      ''
+    + lib.optionalString stdenv.isDarwin ''
+      makeWrapper $out/bin/.gitk-go-unwrapped \
+        $out/bin/gitk-go \
+        ${gitPath}
+    '';
 
   tags = lib.optionals withGitCli [ "gitcli" ];
 
