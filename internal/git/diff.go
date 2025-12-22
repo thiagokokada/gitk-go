42858545640f87d5a99d14fc7a1a7fa6ba6d076b
Thiago Kenji Okada <thiagokokada@gmail.com>
2025-12-22 02:09:20 +0000

    treewide: reorganize files

--- internal/git/diff.go
+++ internal/git/diff.go
@@ -1,6 +1,51 @@
 package git
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
+
+func (s *Service) Diff(commit *Commit) (string, []FileSection, error) {
+	if commit == nil {
+		return "", nil, fmt.Errorf("commit not specified")
+	}
+	header := FormatCommitHeader(commit)
+	diffText, err := s.commitDiffText(commit)
+	if err != nil {
+		return "", nil, err
+	}
+	if strings.TrimSpace(diffText) == "" {
+		return header + "\nNo file level changes.", nil, nil
+	}
+	if !strings.HasSuffix(header, "\n") {
+		header += "\n"
+	}
+	var b strings.Builder
+	b.WriteString(header)
+	b.WriteString(diffText)
+	if !strings.HasSuffix(diffText, "\n") {
+		b.WriteByte('\n')
+	}
+	lineOffset := strings.Count(header, "\n")
+	sections := parseGitDiffSections(diffText, lineOffset)
+	return b.String(), sections, nil
+}
+
+func (s *Service) commitDiffText(commit *Commit) (string, error) {
+	if len(commit.ParentHashes) > 0 {
+		parent := commit.ParentHashes[0]
+		return s.runGitCommand(
+			[]string{"diff", "--no-color", parent, commit.Hash},
+			true,
+			"git diff",
+		)
+	}
+	return s.runGitCommand(
+		[]string{"show", "--no-color", "--pretty=format:", commit.Hash},
+		false,
+		"git show",
+	)
+}
 
 func parseGitDiffSections(diffText string, lineOffset int) []FileSection {
 	lines := strings.Split(diffText, "\n")
