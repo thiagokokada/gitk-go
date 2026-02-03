0e62d6b5d3abd1ec64b15604ed21fa20ef5d461e
Thiago Kenji Okada <thiagokokada@gmail.com>
2026-02-03 16:58:20 +0000

    internal/gui: fix graph canvas box

--- internal/gui/widgets/graph_canvas.go
+++ internal/gui/widgets/graph_canvas.go
@@ -16,9 +16,7 @@ const (
 	graphCanvasLaneMargin  = 6
 	graphCanvasLineWidth   = 2
 
-	graphCanvasLabelPadX  = 4
-	graphCanvasLabelPadY  = 2
-	graphCanvasLabelGap   = 6
+	graphCanvasLabelGap   = 2
 	graphCanvasLabelMinX  = 2
 	graphCanvasConnectorW = 1
 
@@ -563,7 +561,7 @@ func (g *GraphCanvas) drawGraphLabels(
 		}
 		style := graphLabelStyleFor(g.draw.theme, label, nodeColor)
 		textID := g.draw.canvas.CreateText(
-			x+graphCanvasLabelPadX, yMid,
+			x, yMid,
 			Anchor(W),
 			Txt(label),
 			Fill(style.text),
@@ -572,10 +570,10 @@ func (g *GraphCanvas) drawGraphLabels(
 		if len(bbox) < 4 {
 			continue
 		}
-		x1 := tkutil.Atoi(bbox[0]) - graphCanvasLabelPadX
-		y1 := tkutil.Atoi(bbox[1]) - graphCanvasLabelPadY
-		x2 := tkutil.Atoi(bbox[2]) + graphCanvasLabelPadX
-		y2 := tkutil.Atoi(bbox[3]) + graphCanvasLabelPadY
+		x1 := tkutil.Atoi(bbox[0])
+		y1 := tkutil.Atoi(bbox[1])
+		x2 := tkutil.Atoi(bbox[2])
+		y2 := tkutil.Atoi(bbox[3])
 		if x1 >= g.draw.canvasWidth {
 			continue
 		}
